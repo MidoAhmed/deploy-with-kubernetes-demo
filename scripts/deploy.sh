8a15a5f23f07226aa722e8a7b7ce103ba3ab4963
mohamed <med.elkhalki0@gmail.com>
2020-12-30 13:40:02 +0100

    update deployments with the Docker image version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # test kubectl access
+echo "CMD -> kubectl get pods"
 kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
@@ -8,18 +9,19 @@ kubectl \
   --insecure-skip-tls-verify \
   get pods
 
-# kubectl \
-#   --kubeconfig=/dev/null \
-#   --server=$KUBERNETES_SERVER \
-#   --token=$KUBERNETES_TOKEN \
-#   --insecure-skip-tls-verify \
-#   apply -f ./kube/
+echo "CMD -> kubectl apply -f ./.k8s"
+kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --token=$KUBERNETES_TOKEN \
+  --insecure-skip-tls-verify \
+  apply -f ./.k8s/
 
-# echo "The build number is ${TRAVIS_BUILD_NUMBER}"
+echo "The build number is ${TRAVIS_BUILD_NUMBER}"
 
-# kubectl \
-#   --kubeconfig=/dev/null \
-#   --server=$KUBERNETES_SERVER \
-#   --token=$KUBERNETES_TOKEN \
-#   --insecure-skip-tls-verify \
-#   set image deployment/pypytest pypytest=<dockerUser>/pypytest:${TRAVIS_BUILD_NUMBER} --record
+kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --token=$KUBERNETES_TOKEN \
+  --insecure-skip-tls-verify \
+  set image deployment/pypytest pypytest=<dockerUser>/pypytest:${TRAVIS_BUILD_NUMBER} --record
--- scripts/updatekube.sh
+++ scripts/updatekube.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" k8s/deployment.yml
-cat kube/deployment.yml
+echo "update our .k8s/*.deployment.yml files with the Docker image version"
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" .k8s/mongo.deployment.yml .k8s/nginx.deployment.yml .k8s/node.deployment.yml .k8s/redis.deployment.yml
+cat .k8s/*.deployment.yml
