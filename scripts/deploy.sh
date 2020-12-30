8139f8bca47d327e3cbe3a61cd9644620c7e6adc
mohamed <med.elkhalki0@gmail.com>
2020-12-30 16:23:37 +0100

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,9 +19,14 @@ kubectl \
 
 echo "The build number is ${TRAVIS_BUILD_NUMBER}"
 
-# kubectl \
-#   --kubeconfig=/dev/null \
-#   --server=$KUBERNETES_SERVER \
-#   --token=$KUBERNETES_TOKEN \
-#   --insecure-skip-tls-verify \
-#   set image deployment/pypytest pypytest=<dockerUser>/pypytest:${TRAVIS_BUILD_NUMBER} --record
+kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --token=$KUBERNETES_TOKEN \
+  --insecure-skip-tls-verify \
+  set image -f ./.k8s/ \
+  node-codewithdan=$DOCKER_ACCT/node-codewithdan:${TRAVIS_BUILD_NUMBER} \
+  mongo=$DOCKER_ACCT/mongo:${TRAVIS_BUILD_NUMBER} \
+  nginx=$DOCKER_ACCT/nginx:${TRAVIS_BUILD_NUMBER} \
+  redis=$DOCKER_ACCT/redis:${TRAVIS_BUILD_NUMBER} \
+   --record
