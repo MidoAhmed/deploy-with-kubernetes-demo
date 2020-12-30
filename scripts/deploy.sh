2375f39b6149ba77dac130d78694296e2b11a500
mohamed <med.elkhalki0@gmail.com>
2020-12-30 14:32:16 +0100

    test ovrall workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,9 +19,9 @@ kubectl \
 
 echo "The build number is ${TRAVIS_BUILD_NUMBER}"
 
-kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --token=$KUBERNETES_TOKEN \
-  --insecure-skip-tls-verify \
-  set image deployment/pypytest pypytest=<dockerUser>/pypytest:${TRAVIS_BUILD_NUMBER} --record
+# kubectl \
+#   --kubeconfig=/dev/null \
+#   --server=$KUBERNETES_SERVER \
+#   --token=$KUBERNETES_TOKEN \
+#   --insecure-skip-tls-verify \
+#   set image deployment/pypytest pypytest=<dockerUser>/pypytest:${TRAVIS_BUILD_NUMBER} --record
