156607418af24bd1041a858546e553231aea670d
mohamed <med.elkhalki0@gmail.com>
2020-12-30 13:00:47 +0100

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
-/usr/local/bin/kubectl \
+
+# test kubectl access
+kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
-  apply -f ./kube/
-echo "The build number is ${TRAVIS_BUILD_NUMBER}"
-/usr/local/bin/kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
-  --token=$KUBERNETES_TOKEN \
-  set image deployment/pypytest pypytest=<dockerUser>/pypytest:${TRAVIS_BUILD_NUMBER} --record
+  --insecure-skip-tls-verify \
+  get pods
+
+# kubectl \
+#   --kubeconfig=/dev/null \
+#   --server=$KUBERNETES_SERVER \
+#   --token=$KUBERNETES_TOKEN \
+#   --insecure-skip-tls-verify \
+#   apply -f ./kube/
+
+# echo "The build number is ${TRAVIS_BUILD_NUMBER}"
+
+# kubectl \
+#   --kubeconfig=/dev/null \
+#   --server=$KUBERNETES_SERVER \
+#   --token=$KUBERNETES_TOKEN \
+#   --insecure-skip-tls-verify \
+#   set image deployment/pypytest pypytest=<dockerUser>/pypytest:${TRAVIS_BUILD_NUMBER} --record
